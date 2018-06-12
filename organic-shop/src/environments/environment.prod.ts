73e612850dbec2aa7b48c73d88f9198d3b585340
Lyudmyla <glyuda@yahoo.com>
2018-06-12 20:45:53 +0300

    Getting started the project with a navigation bar and a structure

---
+++ organic-shop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDdzDU7JNpAjFGteqiSw90qz9RXp19Wz4M",
+    authDomain: "organic-shop-f0048.firebaseapp.com",
+    databaseURL: "https://organic-shop-f0048.firebaseio.com",
+    projectId: "organic-shop-f0048",
+    storageBucket: "organic-shop-f0048.appspot.com",
+    messagingSenderId: "166015019912"
+  }
+};
